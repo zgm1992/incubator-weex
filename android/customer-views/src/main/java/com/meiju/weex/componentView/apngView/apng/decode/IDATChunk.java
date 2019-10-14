d931063c61ddfe8f070743c24f83a1ecf073b875
jarvan <guoming.zheng@midea.com>
2019-10-14 17:17:07 +0800

    fix: 剔除apng-view  里面涉及到了到了 io.reactivex.Observable  rxjava库件的依赖

--- android/customer-views/src/main/java/com/meiju/weex/componentView/apngView/apng/decode/IDATChunk.java
+++
@@ -1,9 +0,0 @@
-package com.meiju.weex.componentView.apngView.apng.decode;
-
-/**
- * @Description: 作用描述
- * @CreateDate: 2019/7/18
- */
-class IDATChunk extends Chunk {
-    static final int ID = Chunk.fourCCToInt("IDAT");
-}
--- android/customer-views/src/main/java/com/meiju/weex/componentView/apngView/apng/decode/IENDChunk.java
+++
@@ -1,9 +0,0 @@
-package com.meiju.weex.componentView.apngView.apng.decode;
-
-/**
- * @Description: 作用描述
- * @CreateDate: 2019/7/18
- */
-class IENDChunk extends Chunk {
-    static final int ID = Chunk.fourCCToInt("IEND");
-}
